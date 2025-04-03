13d1b259b5f7323ac7f18443d9819fc63bd3da9d
Devashish Chakraborty <devashishchakra@gmail.com>
2025-04-04 02:39:58 +0530

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<Signup />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
